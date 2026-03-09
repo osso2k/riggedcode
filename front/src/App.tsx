878a978cb5fe94147c1b9abf2d00f62a244c7927
osso2k <ossoble817@gmail.com>
2026-03-09 06:21:45 +0300

    Starting riggecode

---
+++ front/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline"> riggedcode </h1>
+      <p className="text-lg text-gray-400">itch the patterns to your brain.</p>
+    </div>
+  )
+}
+
+export default App
